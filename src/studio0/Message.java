a79f4eeecfdef0f33caee08564e27bad84df2472
devinbunner <devinbunner@localhost>
2023-08-31 10:40:29 -0500

    added name

--- src/studio0/Message.java
+++ src/studio0/Message.java
@@ -5,6 +5,7 @@ public class Message {
 	public static void main(String args[]) {
 		System.out.println("Say hi to your group mates!");
 		System.out.println("Hi my name is christien");
+		System.out.println("Hi my name is Devin");
 		
 	}
 }
